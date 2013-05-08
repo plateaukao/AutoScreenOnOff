9047a8c9c7563d2aee4d9bc66fd7e39e943bad6c
Daniel Kao <daniel.kao@gmail.com>
2013-05-09 03:38:16 +0800

    add toggle UI effect for appWidget

--- gen/com/danielkao/poweroff/R.java
+++ gen/com/danielkao/poweroff/R.java
@@ -22,6 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int widget_off=0x7f020001;
+        public static final int widget_on=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090001;
--- src/com/danielkao/poweroff/SensorMonitorService.java
+++ src/com/danielkao/poweroff/SensorMonitorService.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Service;
 import android.app.admin.DevicePolicyManager;
+import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -17,53 +18,52 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
-public class SensorMonitorService extends Service  implements SensorEventListener {
-    // Binder given to clients
-    private final IBinder mBinder = new LocalBinder();
-    
-    private SensorManager mSensorManager;
-    private PowerManager mPowerManager;
-    private Sensor mProximity;
-    
-    private boolean mIsRegistered;
-    
-    private WakeLock partialLock,screenLock;
-    
-    DevicePolicyManager deviceManager;
-    ComponentName mDeviceAdmin;
-    
-    private boolean isActiveAdmin() {
-        return deviceManager.isAdminActive(mDeviceAdmin);
-    }
-    
-    @Override
+public class SensorMonitorService extends Service implements
+		SensorEventListener {
+	// Binder given to clients
+	private final IBinder mBinder = new LocalBinder();
+
+	private SensorManager mSensorManager;
+	private PowerManager mPowerManager;
+	private Sensor mProximity;
+
+	private boolean mIsRegistered;
+
+	private WakeLock partialLock, screenLock;
+
+	DevicePolicyManager deviceManager;
+	ComponentName mDeviceAdmin;
+
+	private boolean isActiveAdmin() {
+		return deviceManager.isAdminActive(mDeviceAdmin);
+	}
+
+	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-    	if(intent==null)
-    		return super.onStartCommand(intent, flags, startId);
-    	
-    	int action = intent.getIntExtra(ConstantValues.SERVICEACTION, -1);
-    	if(action == ConstantValues.SERVICEACTION_TOGGLE)
-    	{
-    		// do the toggle first
-    		togglePreference();
-    		
-    		if(mIsRegistered)
-    		{
-    			unregisterSensor();
-    		}
-    		else
-    		{
-    			registerSensor();
-    		}
-    	}
+		if (intent == null)
+			return super.onStartCommand(intent, flags, startId);
+
+		int action = intent.getIntExtra(ConstantValues.SERVICEACTION, -1);
+		if (action == ConstantValues.SERVICEACTION_TOGGLE) {
+			// do the toggle first
+			togglePreference();
+			updateWidgetUI(intent);
+
+			if (mIsRegistered) {
+				unregisterSensor();
+			} else {
+				registerSensor();
+			}
+		}
 		return super.onStartCommand(intent, flags, startId);
 	}
 
 	//
-    // life cycle
-    //
+	// life cycle
+	//
 	public SensorMonitorService() {
 	}
 
@@ -71,22 +71,24 @@ public class SensorMonitorService extends Service  implements SensorEventListene
 	public IBinder onBind(Intent intent) {
 		return mBinder;
 	}
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-
 		deviceManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 		mDeviceAdmin = new ComponentName(this, TurnOffReceiver.class);
 
-		mPowerManager = ((PowerManager)getSystemService(POWER_SERVICE));
+		mPowerManager = ((PowerManager) getSystemService(POWER_SERVICE));
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-		partialLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "TAG");
+		partialLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+				"TAG");
 		screenLock = mPowerManager.newWakeLock(
-				PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "TAG");
+				PowerManager.ACQUIRE_CAUSES_WAKEUP
+						| PowerManager.FULL_WAKE_LOCK
+						| PowerManager.ON_AFTER_RELEASE, "TAG");
 	}
 
 	@Override
@@ -96,102 +98,131 @@ public class SensorMonitorService extends Service  implements SensorEventListene
 	}
 
 	// to return service class
-    public class LocalBinder extends Binder {
-        SensorMonitorService getService() {
-            return SensorMonitorService.this;
-        }
-    }
-    
-    //
-    // pubilc API for client
-    //
-    public void registerSensor(){
-    	ConstantValues.logv("registerSensor");
-    	if(mIsRegistered){
-    		Toast.makeText(SensorMonitorService.this, "Auto Screen On/off is already turned on", Toast.LENGTH_LONG ).show();
-    		return;
-    	}
+	public class LocalBinder extends Binder {
+		SensorMonitorService getService() {
+			return SensorMonitorService.this;
+		}
+	}
 
-    	mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
-    	mIsRegistered = true;
-    	
-    	if(partialLock!=null)
-    		partialLock.acquire();
+	//
+	// pubilc API for client
+	//
+	public void registerSensor() {
+		ConstantValues.logv("registerSensor");
+		if (mIsRegistered) {
+			Toast.makeText(SensorMonitorService.this,
+					"Auto Screen On/off is already turned on",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
 
-    	Toast.makeText(SensorMonitorService.this, "Turn on Auto Screen On/off", Toast.LENGTH_LONG ).show();
-    }
-    
-    public void unregisterSensor(){
-    	ConstantValues.logv("unregisterSensor");
-        if(mIsRegistered){
-        	mSensorManager.unregisterListener(this);
-        	Toast.makeText(SensorMonitorService.this, "Turn off Auto Screen On/off", Toast.LENGTH_LONG ).show();
-        }
-        
-        if(partialLock!=null) partialLock.release();
-    	mIsRegistered = false;
-    	
-    }
-    
-    public boolean isRegistered(){ return mIsRegistered; }
-    
-    //
-    // listener
-    //
-    
-    @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-    	ConstantValues.logv("onAccuracyChanged");
-    }
+		mSensorManager.registerListener(this, mProximity,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mIsRegistered = true;
 
-    @SuppressLint("Wakelock")
+		if (partialLock != null)
+			partialLock.acquire();
+
+		Toast.makeText(SensorMonitorService.this, "Turn on Auto Screen On/off",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void unregisterSensor() {
+		ConstantValues.logv("unregisterSensor");
+		if (mIsRegistered) {
+			mSensorManager.unregisterListener(this);
+			Toast.makeText(SensorMonitorService.this,
+					"Turn off Auto Screen On/off", Toast.LENGTH_SHORT).show();
+		}
+
+		if (partialLock != null)
+			partialLock.release();
+		mIsRegistered = false;
+
+	}
+
+	public boolean isRegistered() {
+		return mIsRegistered;
+	}
+
+	//
+	// listener
+	//
+
 	@Override
-    public final void onSensorChanged(SensorEvent event) {
-      // The light sensor returns a single value.
-      // Many sensors return 3 values, one for each axis.
-      float lux = event.values[0];
-      // Do something with this sensor value.
-      ConstantValues.logv("onSensorChanged:%f",lux);
-      if(isActiveAdmin())
-      {
-    	  // should turn off
-    	  if(lux == 0f) {
-    		  if(mPowerManager.isScreenOn()){
-    			  deviceManager.lockNow();
-    			  ConstantValues.logv("turn off");
-    		  }
-    	  }
-    	  // should turn on
-    	  else
-    	  {
-    		  if(!mPowerManager.isScreenOn())
-    		  {
-    			  ConstantValues.logv("turn on");
-    			  if(!screenLock.isHeld())
-    			  {
-    				  screenLock.acquire();
-    				  
-    				  new Thread(new Runnable() {
-    					  public void run() {
-    						  try {
-								Thread.sleep(1000);
-							} catch (InterruptedException e) {
-								e.printStackTrace();
+	public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+		ConstantValues.logv("onAccuracyChanged");
+	}
+
+	@SuppressLint("Wakelock")
+	@Override
+	public final void onSensorChanged(SensorEvent event) {
+		// The light sensor returns a single value.
+		// Many sensors return 3 values, one for each axis.
+		float lux = event.values[0];
+		// Do something with this sensor value.
+		ConstantValues.logv("onSensorChanged:%f", lux);
+		if (isActiveAdmin()) {
+			// should turn off
+			if (lux == 0f) {
+				if (mPowerManager.isScreenOn()) {
+					deviceManager.lockNow();
+					ConstantValues.logv("turn off");
+				}
+			}
+			// should turn on
+			else {
+				if (!mPowerManager.isScreenOn()) {
+					ConstantValues.logv("turn on");
+					if (!screenLock.isHeld()) {
+						screenLock.acquire();
+
+						new Thread(new Runnable() {
+							public void run() {
+								try {
+									Thread.sleep(1000);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+								screenLock.release();
 							}
-    						  screenLock.release();
-    					  }
-    				  }).start();
-    			  }
-    		  }
-    	  }
-      }
-    }
-    
-    private void togglePreference(){
-    	SharedPreferences sp = getSharedPreferences(ConstantValues.PREF, Activity.MODE_PRIVATE);
-    	boolean IsAutoOn = sp.getBoolean(ConstantValues.IS_AUTO_ON, false);
-    	Editor editor = sp.edit();
-    	editor.putBoolean(ConstantValues.IS_AUTO_ON, !IsAutoOn);
-    	editor.commit();
-    }
+						}).start();
+					}
+				}
+			}
+		}
+	}
+
+	private void togglePreference() {
+		SharedPreferences sp = getSharedPreferences(ConstantValues.PREF,
+				Activity.MODE_PRIVATE);
+		boolean IsAutoOn = sp.getBoolean(ConstantValues.IS_AUTO_ON, false);
+		Editor editor = sp.edit();
+		editor.putBoolean(ConstantValues.IS_AUTO_ON, !IsAutoOn);
+		editor.commit();
+
+	}
+
+	private void updateWidgetUI(Intent intent) {
+        int widgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
+        if(widgetId == -1)
+        	return;
+        
+		// update UI if necessary
+		SharedPreferences sp = getSharedPreferences(ConstantValues.PREF, Activity.MODE_PRIVATE);
+		boolean autoOn = sp.getBoolean(ConstantValues.IS_AUTO_ON, false);
+
+        AppWidgetManager appWidgetMan = AppWidgetManager.getInstance(this);
+        RemoteViews views = new RemoteViews(this.getPackageName(),R.layout.toggleonoff_appwidget);
+        
+		if (autoOn) {
+			// set icon to on
+			views.setImageViewResource(R.id.imageview, R.drawable.widget_on);
+		} else {
+			// set icon to off
+			views.setImageViewResource(R.id.imageview, R.drawable.widget_off);
+		}
+		
+        appWidgetMan.updateAppWidget(widgetId, views);  
+	}
 }
