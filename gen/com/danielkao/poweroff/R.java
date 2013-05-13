43152f6b816fd38303ed3e3770ff459872647cea
Daniel Kao <daniel.kao@gmail.com>
2013-05-13 11:00:53 +0800

    fine tune widget UI

--- gen/com/danielkao/poweroff/R.java
+++ gen/com/danielkao/poweroff/R.java
@@ -26,8 +26,9 @@ public final class R {
         public static final int widget_on=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
+        public static final int action_settings=0x7f090002;
         public static final int imageview=0x7f090000;
+        public static final int textview=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
