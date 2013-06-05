2af246383ec018c9eb2e9cb4f642026dff48155f
Daniel Kao <daniel.kao@gmail.com>
2013-06-05 11:46:15 +0800

    update prefrencecategory color. change version number

--- src/com/danielkao/autoscreenonoff/MyPreferenceCategory.java
+++ src/com/danielkao/autoscreenonoff/MyPreferenceCategory.java
@@ -1,7 +1,33 @@
 package com.danielkao.autoscreenonoff;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.preference.PreferenceCategory;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by plateau on 2013/06/05.
  */
-public class MyPreferenceCategory {
+public class MyPreferenceCategory extends PreferenceCategory{
+    public MyPreferenceCategory(Context context) {
+        super(context);
+    }
+
+    public MyPreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyPreferenceCategory(Context context, AttributeSet attrs,
+                                int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        TextView titleView = (TextView) view.findViewById(android.R.id.title);
+        titleView.setTextColor(Color.parseColor("#45A0D9"));
+    }
 }
